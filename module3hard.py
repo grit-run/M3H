bee3e0fe38976a439cab6107c1050475d3445c09
elinsky <dmitry.ilinsky@gmail.com>
2024-06-02 16:55:31 +0300

    not sure really fast solution, but more or less universal

---
+++ module3hard.py
@@ -0,0 +1,31 @@
+data_structure = [
+    [1, 2, 3],
+    {'a': 4, 'b': 5},
+    (6, {'cube': 7, 'drum': 8}),
+    "Hello",
+    ((), [{(2, 'Urban', ('Urban2', 35))}])
+]
+
+
+def calculate_structure_sum(any_structure):
+    flat = []
+    sum = 0
+    for i in any_structure:
+        if isinstance(i, (set, list, tuple)):
+            flat.append(calculate_structure_sum(i))
+        elif isinstance(i, dict):
+            for k, v in i.items():
+                flat.append(k)
+                flat.append(v)
+        else:
+            flat.append(i)
+    for i in flat:
+        if isinstance(i, int):
+            sum += i
+        else:
+            sum += len(i)
+    return sum
+
+
+result = calculate_structure_sum(data_structure)
+print(result)
